a82c815822d8ae95adda73c589dcdaf70dcfa854
Antoine Coatanoan <antoinecoatanoan@MacBook-Air-de-Antoine.local>
2026-07-17 11:18:26 +0200

    Add base server config with Express, Pug, Axios and HubSpot client

--- index.js
+++ index.js
@@ -1,71 +1,37 @@
+// Charge les variables d'environnement depuis le fichier .env
+require('dotenv').config();
+
+// Importe le framework Express pour créer le serveur web
 const express = require('express');
+
+// Importe Axios pour faire des requêtes HTTP vers l'API HubSpot
 const axios = require('axios');
+
+// Crée l'application Express
 const app = express();
 
+// Définit le port d'écoute du serveur
+const PORT = 3000;
+
+// Lit le token d'authentification HubSpot depuis les variables d'environnement
+const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_TOKEN;
+
+// Lit l'identifiant du custom object (Robots) depuis les variables d'environnement
+const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
+
+// Configure Pug comme moteur de templates pour générer les pages HTML
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+
+// Indique à Express où trouver les fichiers de vues (dossier views/)
+app.set('views', './views');
+
+// Permet de lire les données envoyées via les formulaires HTML
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// Sert les fichiers statiques (CSS, images) depuis le dossier public/
+app.use(express.static('public'));
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
-
-
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+// Construit l'URL de base pour l'API HubSpot CRM v3 du custom object Robots
+const HUBSPOT_API_BASE = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+// Démarre le serveur sur le port 3000 et affiche un message dans la console
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
